ea9b719a4912438ec478436b3083e39ab7fbf4df
Ibrahima <ibrahima.diallo@eemi.com>
2025-01-19 23:39:24 +0100

    Ajoute des dimensions appropriées au logo sur la page de connexion et ajuste la structure du conteneur.

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -52,9 +52,12 @@ export default function SignInPage() {
       {/* Content */}
       <div className="relative h-full flex flex-col items-center justify-center">
         {/* Logo outside white box */}
+        <div className="flex flex-col items-center mb-7">
           <Image
             src="/img/ICO_LOGO_1.png"
             alt="Logo ICO"
+            width={240} // Add appropriate width
+            height={240} // Add appropriate height
             className="w-60 h-auto"
           />
           <h1 className="text-white text-3xl font-bold">
@@ -156,7 +159,8 @@ export default function SignInPage() {
             </Link>
             .
           </p>
-        </div> 
+        </div>
       </div>
+    </div>
   );
 }
